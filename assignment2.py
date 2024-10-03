52b2677a87af2a85fa4e479d791270b90d0337bd
G. Koenders <163114003+Gilian2002@users.noreply.github.com>
2024-10-02 19:32:02 -0500

    Update assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+#import
+TrainData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv')
+
+#train
+Y = TrainData['meal']
+X = TrainData.drop(columns=['meal', 'id', 'DateTime', 'Total', 'Discounts'])
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+
+#modell
+model = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=42, class_weight="balanced")
+modelFit = model.fit(x_train, y_train)
+
+#test if get point
+print(modelFit)
