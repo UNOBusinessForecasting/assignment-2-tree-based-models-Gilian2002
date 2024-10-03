bf4378062b54ccf3233c1e1194936b46794cad34
G. Koenders <163114003+Gilian2002@users.noreply.github.com>
2024-10-03 13:58:11 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -8,23 +8,27 @@ from sklearn.metrics import accuracy_score
 # Import Data
 TrainData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv')
 TrainData.head()
-print(TrainData.head)
+#print(TrainData.head)
 
 
-Y = TrainData['meal'] 
+Y = TrainData['meal']
 X = TrainData.drop(columns=['meal','id','DateTime'], axis=1)
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=40)
 
-# 
-model = RandomForestClassifier(n_estimators=300, n_jobs=-1, random_state= 44, class_weight = "balanced")
+#
+model = RandomForestClassifier(n_estimators=100, random_state= 40)
 modelFit = model.fit(x_train,y_train)
 
+# view the feature scores
+
+feature_scores = pd.Series(model.feature_importances_, index=x_train.columns).sort_values(ascending=False)
+print(feature_scores)
 
 #
 predict = model.predict(x_test)
 acc = accuracy_score(y_test, predict)
 
-print("Model accuracy is {}%.".format(acc*100)) 
+print("Model accuracy is {}%.".format(acc*100))
 
 
 # PRED Test data
@@ -54,3 +58,12 @@ pred3["predict_meal"] = pred3["predict_meal"].astype(int)
 
 print("\nDecision Tree Predictions on test data:")
 print(pred3.value_counts())
+
+
+from sklearn.metrics import confusion_matrix
+
+# Predict on the training data to get predictions of the same size as y_train
+pred_train = model.predict(x_train)
+cm = confusion_matrix(y_train, pred_train)
+
+print('Confusion matrix\n\n', cm)
