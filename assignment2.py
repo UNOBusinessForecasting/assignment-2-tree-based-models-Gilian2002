eac3e74ca96bac31db1255b752bcdb864076d797
G. Koenders <163114003+Gilian2002@users.noreply.github.com>
2024-10-03 13:31:04 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -3,13 +3,12 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
 
 
 # Import Data
 TrainData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv')
 TrainData.head()
-#print(TrainData.head)
+print(TrainData.head)
 
 
 Y = TrainData['meal'] 
@@ -17,8 +16,10 @@ X = TrainData.drop(columns=['meal','id','DateTime'], axis=1)
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
 
 # 
-model = RandomForestClassifier(n_estimators=200, n_jobs=-1, random_state= 42, class_weight = "balanced")
+model = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state= 42, class_weight = "balanced")
 modelFit = model.fit(x_train,y_train)
+
+
 #
 predict = model.predict(x_test)
 acc = accuracy_score(y_test, predict)
