472e805db51d0cfb5add4593f47995cfd67b0008
G. Koenders <163114003+Gilian2002@users.noreply.github.com>
2024-10-03 13:25:39 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -23,14 +23,6 @@ modelFit = model.fit(x_train,y_train)
 #Testing Test falid model
 scores = cross_val_score(model, X, Y, cv=5)
 print("Cross-validation scores:", scores)
-print("Average score:", scores.mean())
-print("Standard deviation:", scores.std())
-print("Model parameters:", modelFit.get_params())
-print("Classes:", modelFit.classes_)
-print("Number of classes:", modelFit.n_classes_)
-print("Number of features:", modelFit.n_features_in_)
-print("Feature importances:", modelFit.feature_importances_)
-
 
 #
 predict = model.predict(x_test)
