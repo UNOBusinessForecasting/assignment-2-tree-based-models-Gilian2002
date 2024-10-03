08d14a5dbac38bf7fced195d136cc4641290f209
G. Koenders <163114003+Gilian2002@users.noreply.github.com>
2024-10-02 20:16:12 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,33 +1,55 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-#import
+# %%
 TrainData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv')
+TrainData.head()
 
-#train
+# %%
+from sklearn.model_selection import train_test_split
+
 Y = TrainData['meal']
-X = TrainData.drop(columns=['meal', 'id', 'DateTime', 'Total', 'Discounts'])
+X = TrainData.drop(columns=['meal','id','DateTime'], axis=1)
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
 
-#modell
-model = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=42, class_weight="balanced")
-modelFit = model.fit(x_train, y_train)
+# %%
 
+model = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state= 42, class_weight = "balanced")
+modelFit = model.fit(x_train,y_train)
+
+#
 predict = model.predict(x_test)
 acc = accuracy_score(y_test, predict)
-#Model accurate.
-print("Model accuracy is {}%.".format(acc*100))
 
-#import data
+print("Model accuracy is {}%.".format(acc*100)) 
+
+# PRED Test data
 TestData = pd.read_csv('https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv')
-xt = TestData.drop(columns=['id', 'DateTime'])
-xt = xt.reindex(columns=X.columns)
+xt = TestData.drop(columns = ['meal','id','DateTime'], axis =1)
+pred = model.predict(xt)
+print(pred)
 
-#make predictions
-predTest = modelFit.predict(xt)
-preddf = pd.DataFrame(predTest, columns=["predict_meal"])
-preddf["predict_meal"] = preddf["predict_meal"].astype(int)
 
-print(preddf.value_counts())
+# Predict Random forerst
+predTest2 = model.predict(xt) #model is linked from earlier
+
+pred2 = pd.DataFrame(predTest2, columns=["predict_meal"])
+
+pred2["predict_meal"] = pred2["predict_meal"].astype(int)
+
+print("\nRandom Forest Predictions on test data:")
+print(pred2.value_counts())
+
+
+#Predict with decisison Tree
+predTest3 = DecisionTreeClassifier(max_depth=10, min_samples_leaf=10).fit(x_train,y_train).predict(xt)
+
+pred3 = pd.DataFrame(predTest3, columns=["predict_meal"])
+
+pred3["predict_meal"] = pred3["predict_meal"].astype(int)
+
+print("\nDecision Tree Predictions on test data:")
+print(pred3.value_counts())
